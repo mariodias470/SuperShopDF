9bc2be8de4ed5954435b20047070182608b9746f
MarioDias470 <mario.sousa.dias@formandos.cinel.pt>
2026-07-30 23:15:26 +0100

    Modify Products.

--- SuperShopDF.Web/Data/Entities/Products.cs
+++ SuperShopDF.Web/Data/Entities/Products.cs
@@ -16,6 +16,9 @@ namespace SuperShopDF.Web.Data.Entities
     {
         [Key]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} must not exceed {1} characters")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -25,14 +28,15 @@ namespace SuperShopDF.Web.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
 
+        
         [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
         public double Stock { get; set; }
     
