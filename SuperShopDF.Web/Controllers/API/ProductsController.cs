be5e639880a61440e568df19e42f8ebe5bda4d9b
MarioDias470 <mario.sousa.dias@formandos.cinel.pt>
2026-09-09 00:07:45 +0100

    API products controller

---
+++ SuperShopDF.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using SuperShopDF.Web.Data.Entities;
+
+namespace SuperShopDF.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // public class ProductsController: Microsoft.AspNetCore.Mvc.Controller
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _productRepository.GetAll();
+            return Ok(products);
+        }
+
+
+    } // end class ProductsController
+
+} // end namespace SuperShopDF.Web.Controllers.API
